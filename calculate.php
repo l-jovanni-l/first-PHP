e0acb40282bc06910fcdfc3b8beb87934c2ab97c
Jovanni Elias <jovanni.elias@g.austincc.edu>
2020-03-05 20:17:54 -0600

    explanation of how vars are used in php

--- calculate.php
+++ calculate.php
@@ -19,6 +19,20 @@ $response = "error";
     if (!empty($num1) && !empty($num2) && !empty($oper)){
 
         switch ($oper) {
+//  I am a little confused here. Why does the "add" work? 
+//  Does it go into the values of the varaiable from the html file?
+//  
+//  This is what i think is going on now correct me if im wrong. 
+//  The variable $oper is created here on the php file but we actually 
+//  value from the main js file when we created the operator variable on line 32. 
+//  The operator variable is then handed to the sendCalculation() where it 
+//  then called op. When we hand ajax the data we pair them with keys.
+//  On the right side of the " : " is the op variable bc that is from the client side right?
+//  then on the left side of the colon the variable is given the new name "oper".
+//  Oper must match the variable name on this php file im guessing bc i used it on line 14
+//  So now that the data is here finally we have the actual value of wtv check box is checked. 
+//  RIGHTTTTTT. I think I just answered my own question. 
+//    
             case 'add':
                 $response = "This works";
                 break;
