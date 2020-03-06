0c94b447f707bbb966031c1ce02c287432c0d78f
Jovanni Elias <jovanni.elias@g.austincc.edu>
2020-03-05 20:58:46 -0600

    code complete

--- calculate.php
+++ calculate.php
@@ -6,34 +6,38 @@ $response = "error";
 
     if (!empty($_POST)) {
 
-    // Now I get the values from js to php. You could do extra functions 
-    // here like trimming striping the tags (removes <html> tags aand etc)
-    // or set a max amount of chars with substr()
-    $num1 = $_POST['num1'];
-    $num2 = $_POST['num2'];
-    $oper = $_POST['oper'];
+        // Now I get the values from js to php. You could do extra functions 
+        // here like trimming striping the tags (removes <html> tags aand etc)
+        // or set a max amount of chars with substr()
+        $num1 = strip_tags(trim($_POST['num1']));
+        $num2 = strip_tags(trim($_POST['num2']));
+        $oper = strip_tags(trim($_POST['oper']));
 
-    // The cleaning on top may leave a variable empty.
-    // If it is empty stop processing. This will give you the varerror.
+        // The cleaning on top may leave a variable empty.
+        // If it is empty stop processing. This will give you the varerror.
 
-    if (!empty($num1) && !empty($num2) && !empty($oper)){
+        if (!empty($num1) && !empty($num2) && !empty($oper)){
 
-        switch ($oper) {
-        //    The $oper matches the value from the index file. KEY VALUE PAIR!!
-            case 'add':
-                $response = $num1 + $num2;
-                break;
-            case 'sub':
-                $response = $num1 - $num2;
-                break;
+            switch ($oper) {
+            //    The $oper matches the value from the index file. KEY VALUE PAIR!!
+                case 'add':
+                    $response = $num1 + $num2;
+                    break;
+                case 'sub':
+                    $response = $num1 - $num2;
+                    break;
+                case 'mul':
+                    $response = $num1 * $num2;
+                    break;
+                case 'div':
+                    $response = $num1 / $num2;
+                    break;
+                default:
+                    break;
+            }
         }
-
-    }
-
-
     }
     echo $response;
-
 }
 
 
